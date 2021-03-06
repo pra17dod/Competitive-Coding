23df507535efdfb085d69748101ac5211e49c102
pra17dod <pra17dod@gmail.com>
2021-03-07 03:18:44 +0530

    Add GROUPS soln

---
+++ Codechef/March Challenge 2021 Division 3/Chef_and_Groups.cpp
@@ -0,0 +1,76 @@
+//https://www.codechef.com/MARCH21C/problems/GROUPS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int          ll;
+typedef vector<ll>             vec;
+typedef pair<ll,ll >           pll;
+typedef vector<ll>::iterator   itr;
+
+#define P                      pair
+#define F                      first
+#define S                      second 
+#define PB                     push_back 
+#define POB                    pop_back 
+#define EB                     emplace_back
+#define MP                     make_pair
+#define MOD                    1e9 + 7 
+#define INF                    1000000000000000003 
+#define PE(a)                  cout << (a) << endl 
+#define PS(a)                  cout << (a) << " " 
+#define MAX(a, b)              ((a) < (b) ? (b) : (a)) //MIN(3,i++) fail case
+#define MIN(a, b)              ((a) > (b) ? (b) : (a)) //MAX(3,i++) fail case
+#define ALL(v)                 (v).begin(), (v).end()
+#define FOR(a, b, c, s)        for (ll (a) = (b); (a) < (c); (a)+=s)
+#define FORL(a, b, c, s)       for (ll (a) = (b); (a) <= (c); (a)+=s)
+#define FORR(a, b, c, s)       for (ll (a) = (b); (a) >= (c); (a)-=s)
+#define FAST                   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve(){
+    string s;
+    cin >> s;
+    ll cnt=0,f1=0,f2=0;
+    FOR(i,0,s.length(),1){
+        if(s[i]=='1'){
+            if(!f1){
+                cnt++;
+                f1=1;
+            }
+        }
+        else if( s[i]=='0'){
+            f1=0;
+        }
+    }
+    PE(cnt);
+
+}
+
+int main(){
+    FAST;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t=0;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
+
+
+// Sample Input
+// 4
+// 000
+// 010
+// 101
+// 01011011011110
+
+// Sample Output
+// 0
+// 1
+// 2
+// 4
