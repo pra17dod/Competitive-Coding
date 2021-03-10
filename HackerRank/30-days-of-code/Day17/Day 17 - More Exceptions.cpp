a60073a69fad1691c10d634ff4fbc4920f5b5c42
pra17dod <pra17dod@gmail.com>
2021-03-10 10:05:33 +0530

    Add Day 17 Code soln

--- HackerRank/30-days-of-code/Day17/Day 17 - More Exceptions.cpp
+++ HackerRank/30-days-of-code/Day17/Day 17 - More Exceptions.cpp
@@ -8,9 +8,8 @@ using namespace std;
 class Calculator{
     public:  
         int power( int n, int p){
-            if( n < 0 || p < 0){
+            if( n < 0 || p < 0)
                 throw invalid_argument("n and p should be non-negative");
-            }
             else
                 return pow(n,p);
         }
