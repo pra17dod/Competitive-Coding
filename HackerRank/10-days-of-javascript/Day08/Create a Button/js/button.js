0218ded77be07a78e0810c18ac38c23563e47c4d
pra17dod <pra17dod@gmail.com>
2022-01-01 12:04:55 +0530

    Move HackerRank to 00-Different-Platforms

--- HackerRank/10-days-of-javascript/Day08/Create a Button/js/button.js
+++
@@ -1,9 +0,0 @@
-var btn = document.createElement("Button");
-var count =0;
-btn.innerHTML = 0;
-btn.id = "btn";
-btn.className = "btnStyle";
-document.body.appendChild(btn);
-btn.onclick = function() {
-    btn.innerHTML++;
-}
