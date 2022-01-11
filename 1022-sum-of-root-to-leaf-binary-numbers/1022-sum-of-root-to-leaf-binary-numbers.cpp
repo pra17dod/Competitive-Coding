e54c31c00ae6bda18e10326ba17bae3894c72547
Prashant Dodiya <pra17dod@gmail.com>
2022-01-12 02:03:10 +0530

    Time: 3 ms (89.04%), Space: 16.6 MB (72.46%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -11,19 +11,26 @@
  */
 class Solution {
 public:
-    void dfsCall (TreeNode* root, int &ans, int num) {
-        if (root->left == NULL and root->right == NULL) {
-            num = 2*num + root->val;
-            ans += num;
-            return;
-        }
-        num = 2*num + root->val;
-        if (root->left) dfsCall(root->left, ans, num);
-        if (root->right) dfsCall(root->right, ans, num);
-    }
-    int sumRootToLeaf(TreeNode* root) {
-        int ans = 0, num = 0;
-        dfsCall (root, ans, num);
-        return ans;
+    // void dfsCall (TreeNode* root, int &ans, int num) {
+    //     if (root->left == NULL and root->right == NULL) {
+    //         num = 2*num + root->val;
+    //         ans += num;
+    //         return;
+    //     }
+    //     num = 2*num + root->val;
+    //     if (root->left) dfsCall(root->left, ans, num);
+    //     if (root->right) dfsCall(root->right, ans, num);
+    // }
+    // int sumRootToLeaf(TreeNode* root) {
+    //     int ans = 0, num = 0;
+    //     dfsCall (root, ans, num);
+    //     return ans;
+    // }
+    
+    int sumRootToLeaf(TreeNode* root, int sum = 0) {
+        if (root == NULL) return 0;
+        sum = sum*2 + root->val;
+        if (root->left == NULL and root->right == NULL) return sum;
+        return sumRootToLeaf(root->left, sum) + sumRootToLeaf(root->right, sum);            
     }
 };
