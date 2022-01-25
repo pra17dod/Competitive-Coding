2190a174121a64c988929c4c3bff0d58c08029db
Prashant Dodiya <pra17dod@gmail.com>
2022-01-26 02:28:51 +0530

    Time: 4 ms (70.44%), Space: 7.8 MB (48.24%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    void recCall (vector <int> &nums, int id, vector <int> temp, vector <vector <int>> &res) {
-        if (id == nums.size()) return;
-        
+    void recCall (vector <int> &nums, int id, vector <int> temp, vector <vector <int>> &res) { 
+        res.push_back(temp);
         for (int i = id; i < nums.size(); i++) {
             if (i > id and nums[i] == nums[i-1]) continue;
             temp.push_back(nums[i]);
-            res.push_back(temp);
             recCall (nums, i + 1, temp, res);
             temp.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector <vector <int>> res = {{}};
+        vector <vector <int>> res = {};
         vector <int> temp;
         sort(nums.begin(), nums.end());
         recCall (nums, 0, temp, res);
