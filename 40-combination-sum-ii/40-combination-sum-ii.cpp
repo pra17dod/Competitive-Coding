3ae49a9ec9ac0d66680fc76aa71898170ea37436
Prashant Dodiya <pra17dod@gmail.com>
2022-01-26 01:18:44 +0530

    Time: 4 ms (89.84%), Space: 11.4 MB (33.10%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void recCall (vector<int>& cd, int tg, int id, vector <int> vec, vector <vector <int>> &res) {
+    void recCall(vector<int>& cd, int tg, int id, vector <int> tmp, vector <vector <int>> &res) {
         if (tg == 0) {
-            res.push_back(vec);
+            res.push_back(tmp);
             return;
         }
         for (int i = id; i < cd.size(); i++) {
             if (i > id and cd[i] == cd[i-1]) continue;
             if (tg < cd[i]) break;
-            vec.push_back(cd[i]);
-            recCall (cd, tg - cd[i], i + 1, vec, res);
-            vec.pop_back();
-        }
+            tmp.push_back(cd[i]);
+            recCall (cd, tg - cd[i], i+1, tmp, res);
+            tmp.pop_back();
+        }        
     }
-    
-    vector<vector<int>> combinationSum2(vector<int> candidates, int target) {
-        vector <vector < int >> res;
-        vector < int > vec;
-        sort(candidates.begin(), candidates.end());
-        recCall (candidates, target, 0, vec, res);
+    vector<vector<int>> combinationSum2(vector<int>& cd, int tg) {
+        vector <vector <int>> res;
+        vector <int> tmp;
+        sort(cd.begin(), cd.end());
+        recCall(cd, tg, 0, tmp, res);
         return res;
     }
 };
