0db20c3468578f9a391730d0768d6b29325f1975
Prashant Dodiya <pra17dod@gmail.com>
2022-01-30 01:31:29 +0530

    Time: 200 ms (35.67%), Space: 63.2 MB (66.85%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxArea = 0;
+        stack<int> st;
+        
+        for(int i = 0; i <= n; i++){
+            int currHeight = i == n ? 0 : heights[i];
+            // check if currHeight becomes greater then height[top] element of stack. we do a push because it's an increasing sequence
+            // otherwise we do pop and find area, so for that we write a while loop
+            while(!st.empty() && currHeight < heights[st.top()]){
+                int top = st.top(); st.pop(); // current element on which we are working
+                // now we need width & area
+                int width = st.empty() ? i : i - st.top() - 1; // width differ if we stack is empty or not empty after we pop the element
+                int area = heights[top] * width; // current height * width
+                maxArea = max(area, maxArea);
+            }
+            // if it doesn't enter in while loop, it means it's an increasing sequence & we need to push index
+            st.push(i);
+        }
+        return maxArea;
+    }
+};
