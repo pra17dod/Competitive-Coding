78af27d73e1029e8c9c283fca13a51181565a7d8
Prashant Dodiya <pra17dod@gmail.com>
2022-02-02 15:32:24 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.59%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,10 +1,28 @@
-vector <int> vec(46, -1);
+// Space O(n)
+
+// vector <int> vec(46, -1);
+
+// class Solution {   
+// public:    
+//     int climbStairs(int n) {
+//         if (n < 3) return n;
+//         if (vec[n] != -1) return vec[n];
+//         else return vec[n] = climbStairs(n-1) + climbStairs(n-2);
+//     }
+// };
+
+// Space O(1)
 
 class Solution {   
 public:    
     int climbStairs(int n) {
         if (n < 3) return n;
-        if (vec[n] != -1) return vec[n];
-        else return vec[n] = climbStairs(n-1) + climbStairs(n-2);
+        int n1 = 1, n2 = 2;
+        n-=2;
+        while (n--) {
+            n2 = n1 + n2;
+            n1 = n2 - n1;
+        }
+        return n2;
     }
 };
