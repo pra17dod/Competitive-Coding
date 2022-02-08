87760661c071d966a97976d144ae3ca1ae096e2a
Prashant Dodiya <pra17dod@gmail.com>
2022-02-09 03:54:58 +0530

    Time: 6 ms (15.03%), Space: 6 MB (25.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int addDigits(int num) {
-        string s = to_string(num);
-        while (s.length() > 1) {
-            int sum = 0;
-            for (auto &i : s) {
-                sum += i - '0';
-            }
-            s = to_string(sum);
-        }
-        return stoi(s);
+        // string s = to_string(num);
+        // while (s.length() > 1) {
+        //     int sum = 0;
+        //     for (auto &i : s) {
+        //         sum += i - '0';
+        //     }
+        //     s = to_string(sum);
+        // }
+        // return stoi(s);
+        return 1 + (num -1) % 9;
     }
 };
