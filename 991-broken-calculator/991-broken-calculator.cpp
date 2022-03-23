770c9c970f8d3c28275eaf299e1508f34cdd571c
Prashant Dodiya <pra17dod@gmail.com>
2022-03-24 01:32:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.88%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if (target <= startValue) {
+            return startValue - target;
+        }
+        if (target % 2 == 0) {
+            return 1 + brokenCalc(startValue, target/2);
+        }
+        else return 1 + brokenCalc(startValue, target+1);
+    }
+};
