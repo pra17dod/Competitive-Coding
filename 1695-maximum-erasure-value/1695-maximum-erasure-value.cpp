78b3640f63eac672e1c9cfa540f87e051b6bb481
Prashant Dodiya <pra17dod@gmail.com>
2022-06-13 01:07:51 +0530

    Time: 273 ms (90.09%), Space: 99.5 MB (88.88%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        map <int, int> ps;
-        priority_queue <int> pq;
-        int i = 1, j = 1;
-        while (j <= nums.size()+1  and i <= j) {
-            if (j == nums.size()+1 or ps[nums[j-1]] != 0) {
-                // cout << i << " " << j << endl;
-                // cout << ps[nums[j-1]] << endl;
-                pq.push(accumulate(nums.begin() + i - 1, nums.begin() + j - 1, 0));
-                if (j != nums.size() + 1) {
-                    while (i < ps[nums[j-1]] + 1) {
-                        ps[nums[i-1]] = 0;
-                        i++;
-                    }
-                    ps[nums[j-1]] = j;
-                }
+        int n = nums.size();
+        int L = 0;
+        int R = 0;
+        
+        unordered_map<int, int> freqMap;
+        int sum = 0;
+        int ans = 0;
+        while(R < n) {
+            int right = nums[R++];
+            sum += right;
+            freqMap[right]++;
+            
+            while(freqMap[right] > 1) {
+                int left = nums[L++];
+                freqMap[left]--;
+                sum -= left;
             }
-            else {
-                ps[nums[j-1]] = j;
-            }
-            j++;
+            ans = max(ans, sum);
         }
-        return pq.top();  
+        return ans;
     }
 };
