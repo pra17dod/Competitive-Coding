cd7af8a7157fb72e40589933aff31b8742eeaab5
Prashant Dodiya <pra17dod@gmail.com>
2022-08-11 15:14:43 +0530

    Time: 7 ms (70.34%), Space: 9.1 MB (7.43%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool rec(vector <char> &s, int i, int j) {
+        if (i > j) return true;
+        
+        if (s[i] == s[j])
+        {
+            if (rec(s, i+1, j-1) == true)
+                return true;
+        }
+        
+        return false;
+    }
+    
+public:
+    bool isPalindrome(string s) {
+        vector <char> str;
+        for (auto &i : s) {
+            i = tolower(i); 
+            if (ispunct(i) or i == ' ') 
+                continue;
+            str.push_back(i);
+        }
+        int j = str.size() - 1;
+        return rec(str, 0, j);
+    }
+};
