5482559104e6be74ac3136130eb08b9768cba493
Prashant Dodiya <pra17dod@gmail.com>
2022-08-12 00:53:49 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -12,7 +12,7 @@ public:
         }
         
         for (int i = 3; i <= n; i++) {
-            t[i] = max(nums[i-1] + t[i-3], max(nums[i-1] + t[i-2], t[i-1]));
+            t[i] = max(nums[i-1] + t[i-2], t[i-1]);
         }
         
         return t[n];
