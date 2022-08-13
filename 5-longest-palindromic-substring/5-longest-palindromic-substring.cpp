79c0a301b298c09ec902075401f43e02d12a7764
Prashant Dodiya <pra17dod@gmail.com>
2022-08-13 13:47:26 +0530

    Time: 1650 ms (5.00%), Space: 137 MB (24.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int t[1005][1005];
     
-    string lcs(string a, string b) {
+    string lps(string a, string b) {
         memset (t, 0, sizeof(t));
         int n = a.size();
-        priority_queue < pair <int, pair <int, int>> > pq;
+        // maintaining a priority queue for {length, index} of substring used while generating longest substring
+        priority_queue < pair <int, int> > pq;
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (a[i-1] == b[j-1]) {
                     t[i][j] = 1 + t[i-1][j-1];
                     if (t[i][j] > 1) {
-                        pq.push({t[i][j], {i, j}});
+                        // there might be the case when the longest substring is not the palindrome so storing
+                        // substring with length > 2 as len = 1 is always a palindrome and can return any letter
+                        pq.push({t[i][j], i});
                     }
                 }
                 else t[i][j] = 0;
@@ -26,21 +29,25 @@ public:
             pq.pop();
             
             int len = s.first;
-            int i = s.second.first, j = s.second.second;
+            int i = s.second;
             
-            
+            // to generate substring
             for (int k = 0; k < len; k++) {
+                // checking for palindromic substring
                 if (a[i-1 - k] == a[i-1 - (len -1 -k)]) {
                     op.push_back(a[i-1 - k]);
                 }
             }
-                        
+            
+            // if op.size() < len of substring then it is not palindrome we have to check for another substring
             if (op.size() == len) {
+                // if we found the ans then break
                 break;
             }
+            // for next iteration
             else op.clear();
         }
-        
+        // if there was not palindromic substring of size > 1
         if (op.empty()) {
             op = a[0];
         }
@@ -49,21 +56,12 @@ public:
             
     }
     
-    bool isPalindrome(string s, int st) {
-        if (st > s.size()/2) return true;
-        
-        if (s[st] == s[s.size() - st - 1]) 
-            return isPalindrome(s, st+1);
-        
-        else return false;
-    }
-    
     string longestPalindrome(string s) {
         if (s.empty())
             return "";
         
         string p = s; 
         reverse(p.begin(), p.end());
-        return lcs (s, p);
+        return lps (s, p);
     }
 };
