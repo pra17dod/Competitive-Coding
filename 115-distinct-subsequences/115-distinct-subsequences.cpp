5e22c89cda5507644efb892183665cbd27a82d17
Prashant Dodiya <pra17dod@gmail.com>
2022-09-07 01:30:25 +0530

    Time: 21 ms (95.58%), Space: 6.4 MB (96.25%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -19,14 +19,6 @@ public:
             }
         }
         
-        
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= m; j++) {
-        //         cout << t[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        
         return t[m];
     }
     
