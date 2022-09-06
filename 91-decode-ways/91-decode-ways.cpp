15d7befd76a10c9a4758c3f4e31fd8da3f29a267
Prashant Dodiya <pra17dod@gmail.com>
2022-09-06 21:23:11 +0530

    Time: 3 ms (61.76%), Space: 6.1 MB (93.12%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,38 +2,52 @@ class Solution {
 public:
     int t[105];
     
-    int solve(string s, int i) {
-        int n = s.size();
-        if (i == n) {
-            return 1;
-        }
+//     int solve(string s, int i) {
+//         int n = s.size();
+//         if (i == n) {
+//             return 1;
+//         }
         
-        if (t[i] != 0) {
-            return t[i];
-        }
+//         if (t[i] != 0) {
+//             return t[i];
+//         }
         
-        if (s[i] == '0') {
-            return 0;
-        }
+//         if (s[i] == '0') {
+//             return 0;
+//         }
         
-        int l1 = 0, l2 = 0;
+//         int l1 = 0, l2 = 0;
         
-        if (i <= n - 2) {
-            int k = stoi(s.substr(i,2));
-            if (k <= 26 and k > 0) {
-                l2 = solve(s, i+2);
-            } 
-        }
-        l1 = solve(s, i+1);
-        return t[i] = l1 + l2; 
-    } 
+//         if (i <= n - 2) {
+//             int k = stoi(s.substr(i,2));
+//             if (k <= 26 and k > 0) {
+//                 l2 = solve(s, i+2);
+//             } 
+//         }
+//         l1 = solve(s, i+1);
+//         return t[i] = l1 + l2; 
+//     } 
     
     
     int numDecodings(string s) {
         if (s.empty()) return 0;
+        if (s[0] == '0') return 0;
+        
+        int n = s.size();
         
         memset(t, 0, sizeof(t));
     
-        return solve(s, 0);       
+        t[0] = t[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            if (s[i-1] != '0') 
+                t[i] += t[i-1];
+            int k = stoi(s.substr(i-2, 2));
+            if (k >= 10 and k <= 26) 
+                    t[i] += t[i-2];
+            
+        }
+        
+        return t[n];       
     }
 };
