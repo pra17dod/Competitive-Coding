286a910cc99a824701b99f2d56b0082e3987a20e
Prashant Dodiya <pra17dod@gmail.com>
2022-09-07 01:29:08 +0530

    Time: 26 ms (93.37%), Space: 6.4 MB (96.25%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    unsigned long long t[1005][1005];
+    unsigned long long t[1005];
     
     unsigned long long lcs(string s, string p) {
         memset(t, 0, sizeof(t));
         
         int n = s.size(), m = p.size();
         
-        
-        for (int j = 0; j <= m; j++) {
-            t[0][j] = 0;
-        }
-        for (int i = 0; i <= n; i++) {
-            t[i][0] = 1;
-        }
+        t[0] = 1;
         
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = m; j >= 1; j--) {
+                // rev direction bcoz then only we will be able to use
+                // t[i]
                 if (s[i-1] == p[j-1]) {
-                    t[i][j] = t[i-1][j] + t[i-1][j-1];
-                }
-                else {
-                    t[i][j] = t[i-1][j];
+                    t[j] = t[j-1] + t[j];
                 }
             }
         }
         
-        return t[n][m];
+        
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         cout << t[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        return t[m];
     }
     
     
