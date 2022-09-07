bd54e6be3dfcabcc41d91046517d670a6c82c33c
Prashant Dodiya <pra17dod@gmail.com>
2022-09-07 17:51:05 +0530

    Time: 21 ms (16.62%), Space: 13.1 MB (59.50%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,71 @@
 class Solution {
 public:
-    map <string, int> mp;
     
-    int solve (vector<int>& prices, int i, int b) {
-        if (i == prices.size()) return 0;
-
-        string key = to_string(i) + " " + to_string(b);
+//      Top Down Space Optimization
+    
+    int solve(vector<int>& prices) {
+        int n = prices.size();
         
-        if (mp.find(key) != mp.end()) {
-            return mp[key];
-        }  
+        vector <int> prev(2, 0), curr(2, 0);
         
-        if (b) {
-            return mp[key] = max(-prices[i] + solve(prices, i + 1, 0),
-                        solve(prices, i + 1, 1));
-        }
-        else return mp[key] = max(prices[i] + solve(prices, i+1, 1),
-                        solve(prices, i + 1, 0));
+        for(int i = n-1; i >= 0; i--) {
+            curr[1] = max(-prices[i] + prev[0], prev[1]);
+            curr[0] = max(prices[i] + prev[1], prev[0]);   
+            
+            prev = curr;
+        } 
+        return prev[1];
     }
     
+    
+//     Top Down
+//     int solve(vector<int>& prices) {
+//         int n = prices.size();
+        
+//         int t[n+1][2];
+//         memset(t, 0, sizeof(t));
+        
+//         t[n][0] = t[n][1] = 0;
+        
+//         for(int i = n-1; i >= 0; i--) {
+//             t[i][1] = max(-prices[i] + t[i+1][0], t[i+1][1]);
+//             t[i][0] = max(prices[i] + t[i+1][1], t[i+1][0]);    
+//         } 
+//         return (t[0][1]);
+//     }
+    
+    
+//     Recursion + Memoization    
+//     map <string, int> mp;
+    
+//     int solve (vector<int>& prices, int i, int b) {
+//         if (i == prices.size()) return 0;
+
+//         string key = to_string(i) + " " + to_string(b);
+        
+//         if (mp.find(key) != mp.end()) {
+//             return mp[key];
+//         }  
+        
+//         if (b) {
+//             return mp[key] = max(-prices[i] + solve(prices, i + 1, 0),
+//                         solve(prices, i + 1, 1));
+//         }
+//         else return mp[key] = max(prices[i] + solve(prices, i + 1, 1),
+//                         solve(prices, i + 1, 0));
+//     }
+    
     int maxProfit(vector<int>& prices) {
-        return solve(prices, 0, 1);
+//         O(n) Iterative
+        
+//         int profit = 0;
+//         for (int i = 1; i < prices.size(); i++) {
+//             if (prices[i-1] < prices[i]) {
+//                 profit += prices[i] - prices[i-1];
+//             }
+//         }
+//         return profit;
+        
+        return solve(prices);
     }
 };
